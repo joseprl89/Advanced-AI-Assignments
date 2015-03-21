5dd1e519f60231eee2484c4c489a6de60c5056da
Josep <joseprl89@gmail.com>
2015-03-21 18:52:47 +0000

    Added read web data python code so we can read from webs.data into a dictionary

---
+++ IAAPRAC/PAC1/src/ReadWebData.py
@@ -0,0 +1,20 @@
+
+def readRatings(filename="u.data"):
+    # Reads all the lines into a vector of vectors
+    lines = [(l.strip()).split("\t")
+        for l in (open(filename).readlines())]
+    
+    # Creates a dictionary with the id as the first item of the line.
+    dictio = {int(l[0]) : {}  for l in lines}
+   
+    # Populate the dictionary with for each web, a dictionary for each user
+    # containing the rest of values in an array
+    for l in lines:
+        webId = int(l[0])
+        userId = int(l[1])
+        values = [int(s) for s in l[2:]]
+        dictio[webId][userId] = values
+    
+    return dictio
+
+# Example: readRatings("../data/webs.data")
