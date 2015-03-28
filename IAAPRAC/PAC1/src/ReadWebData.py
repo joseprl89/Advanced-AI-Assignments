2ee060b72ceb98839ed9df8b1580ce457a567d79
Josep <joseprl89@gmail.com>
2015-03-28 11:11:26 +0000

    Using the web id to identify the records rather than the user Id as we group the webs not the user in activity 2.

--- IAAPRAC/PAC1/src/ReadWebData.py
+++ IAAPRAC/PAC1/src/ReadWebData.py
@@ -4,7 +4,7 @@ def readRatings(filename="u.data"):
         for l in (open(filename).readlines())]
     
     # Creates a dictionary with the id as the first item of the line.
-    dictio = { int(l[1]) : {}  for l in lines}
+    dictio = { int(l[0]) : {}  for l in lines}
    
     # Populate the dictionary with for each web, a dictionary for each user
     # containing the rest of values in an array
@@ -12,6 +12,6 @@ def readRatings(filename="u.data"):
         webId = int(l[0])
         userId = int(l[1])
         values = [int(s) for s in l[2:]]
-        dictio[userId][webId] = values
+        dictio[webId][userId] = values
     
     return dictio
