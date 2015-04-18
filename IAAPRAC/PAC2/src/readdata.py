5525fef80bebc73912c8e5a11ee823ce66961a98
Josep Rodríguez <joseprl89@gmail.com>
2015-04-18 20:23:29 +0100

    Updated readdata py to use numpy fortran arrays

--- IAAPRAC/PAC2/src/readdata.py
+++ IAAPRAC/PAC2/src/readdata.py
@@ -1,13 +1,21 @@
+from numpy import array,asfortranarray
 
 # Returns a matrix with the wholesale customer data. In each row we have data for a customer
 def readWholeSaleCustomersData(filename="../data/Wholesale customers.csv"):
+    
     # Reads all the lines into a vector of vectors
-    lines = [(l.strip()).split("\t")
+    lines = [(l.strip()).split(",")
         for l in (open(filename).readlines())]
-    
-    # Remove the Title from the csv file
-    lines = lines[1:]
-    
-    return lines
 
-# readWholeSaleCustomersData()
+    # Remove the header
+    lines = lines[1:]
+
+    # Convert all values to floats      
+    for line in lines:
+        for idx, val in enumerate(line):
+            line[idx] = float(val)
+
+    # Return a fortran array from numpy
+    return asfortranarray(array(lines))
+
+# print(readWholeSaleCustomersData())
