1fab5f80b7b66b4fe85c4da9e881edd0c99fd7ae
Josep Rodríguez <joseprl89@gmail.com>
2015-04-19 02:15:54 +0100

    Added other exercises (long time no commit :/ )

---
+++ IAAPRAC/PAC2/src/distanceCalculation.py
@@ -0,0 +1,24 @@
+from math import sqrt
+
+def euclideanDistance(v1,v2):
+    squaredSum = 0
+    for _,val1 in enumerate(v1):
+        for _,val2 in enumerate(v2):
+            squaredSum += pow(val1-val2,2)
+
+    return sqrt(squaredSum)
+
+def pearson(v1,v2):
+    # Simple sums
+    sum1=sum(v1)
+    sum2=sum(v2)
+    # Sums of the squares
+    sum1Sq=sum([pow(v,2) for v in v1])
+    sum2Sq=sum([pow(v,2) for v in v2])
+    # Sum of the products
+    pSum=sum([v1[i]*v2[i] for i in range(len(v1))])
+    # Calculate r (Pearson score)
+    num=pSum-(sum1*sum2/len(v1))
+    den=sqrt((sum1Sq-pow(sum1,2)/len(v1))*(sum2Sq-pow(sum2,2)/len(v1)))
+    if den==0: return 0
+    return 1.0-num/den
--- IAAPRAC/PAC2/src/main.py
+++ IAAPRAC/PAC2/src/main.py
@@ -1,7 +1,9 @@
-from PAC2.src.readdata import readWholeSaleCustomersData
 import matplotlib.pyplot as plot
 import pylab
-from numpy import set_printoptions, dot, linalg, argsort, zeros, allclose
+from PAC2.src.readdata import readWholeSaleCustomersData
+from PAC2.src import distanceCalculation
+from PAC2.src.scaledown import scaledown
+from numpy import set_printoptions, dot, linalg, argsort, zeros, allclose, array
 from _dummy_thread import exit
 from mpl_toolkits.mplot3d import Axes3D
 
@@ -35,6 +37,10 @@ def exercise1():
     pylab.gcf().canvas.set_window_title('Eigenvalues')
     pylab.show()
     
+    print("Eigenvalues")
+    print(valp1)
+    print("EigenVectors")
+    print(vecp1)
     
     # Project all data to the new base defined by the eigenvectors
     d_PCA = zeros((d.shape[0],d.shape[1]))
@@ -79,7 +85,6 @@ def exercise1():
         # Compare rebuilt data to initial one with relative tolerance of 0.05
         if allclose(d,d_recon2,rtol=0.05):
             print("Managed to obtain a 95% accuracy by using " + str(eigenvectorIndexUsed) + " eigenvector values")
-            break
         else:
             print("Didn´t obtain a 95% accuracy by using " + str(eigenvectorIndexUsed) + " eigenvector values")
     
@@ -90,7 +95,225 @@ def exercise1():
         pylab.gcf().canvas.set_window_title('Reconstruction with ' + str(eigenvectorIndexUsed) + ' eigenvector values')
         plot.show()
         
-exercise1()
+def exercise2():
+    # Read data
+    d = readWholeSaleCustomersData()
+    
+    # display the grap:
+    fig1 = plot.figure()
+    sp = fig1.gca(projection = '3d')
+    sp.scatter(d[:,0],d[:,1],d[:,2])
+    fig1.canvas.set_window_title('Initial matrix graph')
+    plot.show()
+    
+    # ANALISIS PCA:
+    # Step 1: Calculate covariance matrix (N x N):
+    d1 = d - d.mean(0)
+    matcov = dot(d1.transpose(),d1)
+    
+    # Step 2: Obtain Eigenvalue and eigenvectors of covariance matrix
+    valp1,vecp1 = linalg.eig(matcov)
+    
+    # Step 3: Decide which vectors are relevant and plot in 2d the eigenvalues decreasing:
+    ind_creciente = argsort(valp1) # orden creciente 
+    ind_decre = ind_creciente[::-1] # orden decreciente 
+    val_decre = valp1[ind_decre] # valores propios en orden decreciente
+    vec_decre = vecp1[:,ind_decre] # ordenar tambien vectores propios
+    pylab.plot(val_decre,'o-')
+    pylab.gcf().canvas.set_window_title('Eigenvalues')
+    pylab.show()
+    
+    print("Eigenvalues")
+    print(valp1)
+    print("EigenVectors")
+    print(vecp1)
+    
+    # Project all data to the new base defined by the eigenvectors
+    d_PCA = zeros((d.shape[0],d.shape[1]))
+    for i in range(d.shape[0]):
+        for j in range(d.shape[1]):
+            d_PCA[i,j] = dot(d1[i,:],vecp1[:,j])
+    
+    # Recover initial data by inverting the projection
+    orig_means = d.mean(0)
+    d_recon = zeros((d.shape[0],d.shape[1]))
+    for i in range(d.shape[0]):
+        d_recon[i] = orig_means
+        for j in range(d.shape[1]):
+            d_recon[i] += d_PCA[i,j]*vecp1[:,j]
+    
+    # Verify that the original data is obtained
+    if allclose(d,d_recon):
+        print("Reconstruction successful")
+    else:
+        print("Failed to reconstruct the initial data")
+        exit(10)
+    
+    dictionaryByRegion = {
+                          "1.0":[],
+                          "2.0":[],
+                          "3.0":[]
+                          }
+    dictionaryByChannel = {
+                           "1.0":[],
+                           "2.0":[]
+                           }
+    
+    # Project data
+    d_PCA2 = zeros((d.shape[0],4))
+    for i in range(d.shape[0]):
+        for j in range(0,2):
+            d_PCA2[i,j] = dot(d1[i,:],vec_decre[:,j])
+        
+    # Rebuild on partial data
+    d_recon2 = zeros((d.shape[0],d.shape[1]))
+    for i in range(d.shape[0]):
+        d_recon2[i] = orig_means
+        for j in range(0,2):
+            d_recon2[i] += d_PCA2[i,j]*vec_decre[:,j]
+        # populate the channel and region dictionary
+        dictionaryByChannel[str(d[i][0])].append(d_recon2[i])
+        dictionaryByRegion[str(d[i][1])].append(d_recon2[i])
+
+    
+    # Graphic representation of the reconstruction by channel
+    channel1Array = array(dictionaryByChannel["1.0"])    
+    channel2Array = array(dictionaryByChannel["2.0"])    
+
+    plot.scatter(channel1Array[:,0],channel1Array[:,1],c="r", marker="x")
+    plot.scatter(channel2Array[:,0],channel2Array[:,1],c="b", marker="x")
+    pylab.gcf().canvas.set_window_title('2D scatter plot by channel')
+    plot.show()
+
+    # Graphic representation of the reconstruction by region
+    region1Array = array(dictionaryByRegion["1.0"])    
+    region2Array = array(dictionaryByRegion["2.0"])    
+    region3Array = array(dictionaryByRegion["3.0"])    
+
+    plot.scatter(region1Array[:,0],region1Array[:,1],c="r", marker="x")
+    plot.scatter(region2Array[:,0],region2Array[:,1],c="b", marker="x")
+    plot.scatter(region3Array[:,0],region3Array[:,1],c="g", marker="x")
+    pylab.gcf().canvas.set_window_title('2D scatter plot by region')
+    plot.show()
+
+def exercise3():
+    # Read data
+    d = readWholeSaleCustomersData()
+    
+    # display the grap:
+    fig1 = plot.figure()
+    sp = fig1.gca(projection = '3d')
+    sp.scatter(d[:,0],d[:,1],d[:,2])
+    fig1.canvas.set_window_title('Initial matrix graph')
+    plot.show()
+    
+    # ANALISIS PCA:
+    # Step 1: Calculate covariance matrix (N x N):
+    d1 = d - d.mean(0)
+    matcov = dot(d1.transpose(),d1)
+    
+    # Step 2: Obtain Eigenvalue and eigenvectors of covariance matrix
+    valp1,vecp1 = linalg.eig(matcov)
+    
+    # Step 3: Decide which vectors are relevant and plot in 2d the eigenvalues decreasing:
+    ind_creciente = argsort(valp1) # orden creciente 
+    ind_decre = ind_creciente[::-1] # orden decreciente 
+    val_decre = valp1[ind_decre] # valores propios en orden decreciente
+    vec_decre = vecp1[:,ind_decre] # ordenar tambien vectores propios
+    pylab.plot(val_decre,'o-')
+    pylab.gcf().canvas.set_window_title('Eigenvalues')
+    pylab.show()
+    
+    print("Eigenvalues")
+    print(valp1)
+    print("EigenVectors")
+    print(vecp1)
+    
+    # Project all data to the new base defined by the eigenvectors
+    d_PCA = zeros((d.shape[0],d.shape[1]))
+    for i in range(d.shape[0]):
+        for j in range(d.shape[1]):
+            d_PCA[i,j] = dot(d1[i,:],vecp1[:,j])
+    
+    # Recover initial data by inverting the projection
+    orig_means = d.mean(0)
+    d_recon = zeros((d.shape[0],d.shape[1]))
+    for i in range(d.shape[0]):
+        d_recon[i] = orig_means
+        for j in range(d.shape[1]):
+            d_recon[i] += d_PCA[i,j]*vecp1[:,j]
+    
+    # Verify that the original data is obtained
+    if allclose(d,d_recon):
+        print("Reconstruction successful")
+    else:
+        print("Failed to reconstruct the initial data")
+        exit(10)
+    
+    dictionaryByRegion = {
+                          "1.0":[],
+                          "2.0":[],
+                          "3.0":[]
+                          }
+    dictionaryByChannel = {
+                           "1.0":[],
+                           "2.0":[]
+                           }
+    
+    firstComponent = 1
+    
+    # Project data
+    d_PCA2 = zeros((d.shape[0],d.shape[1]))
+    for i in range(d.shape[0]):
+        for j in range(firstComponent,firstComponent + 2):
+            d_PCA2[i,j] = dot(d1[i,:],vec_decre[:,j])
+        
+    # Rebuild on partial data
+    d_recon2 = zeros((d.shape[0],d.shape[1]))
+    for i in range(d.shape[0]):
+        d_recon2[i] = orig_means
+        for j in range(firstComponent,firstComponent + 2):
+            d_recon2[i] += d_PCA2[i,j]*vec_decre[:,j]
+        # populate the channel and region dictionary
+        dictionaryByChannel[str(d[i][0])].append(d_recon2[i])
+        dictionaryByRegion[str(d[i][1])].append(d_recon2[i])
+
+    
+    # Graphic representation of the reconstruction by channel
+    channel1Array = array(dictionaryByChannel["1.0"])    
+    channel2Array = array(dictionaryByChannel["2.0"])    
+
+    plot.scatter(channel1Array[:,0],channel1Array[:,1],c="r", marker="x")
+    plot.scatter(channel2Array[:,0],channel2Array[:,1],c="b", marker="x")
+    pylab.gcf().canvas.set_window_title('2D scatter plot by channel')
+    plot.show()
+
+    # Graphic representation of the reconstruction by region
+    region1Array = array(dictionaryByRegion["1.0"])    
+    region2Array = array(dictionaryByRegion["2.0"])    
+    region3Array = array(dictionaryByRegion["3.0"])    
+
+    plot.scatter(region1Array[:,0],region1Array[:,1],c="r", marker="x")
+    plot.scatter(region2Array[:,0],region2Array[:,1],c="b", marker="x")
+    plot.scatter(region3Array[:,0],region3Array[:,1],c="g", marker="x")
+    pylab.gcf().canvas.set_window_title('2D scatter plot by region')
+    plot.show()
+
+def exercise4():
+    # Read data
+    d = readWholeSaleCustomersData()
+        
+    # Scale down using MDS the dimensions
+    #mds = array(scaledown(d,distanceCalculation.euclideanDistance))
+    mds = array(scaledown(d,distanceCalculation.pearson))
+    
+    # Representaci�n de los datos en el espacio 2D MDS:
+    fig1 = pylab.figure()
+    pylab.scatter(mds[:,0],mds[:,1],marker='o',c='r')
+    fig1.suptitle('MDS - Metrica Pearson')
+    pylab.show()
+
+exercise4()
 
 
 
