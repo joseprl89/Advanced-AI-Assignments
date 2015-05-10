d4ed7f0edeec04adba57327a0e84040aeb4b7942
Josep Rodríguez <joseprl89@gmail.com>
2015-05-10 12:57:06 +0100

    Added KNN method and added a loop to plot the results depending on the ratio of tests vs training set sizes.

--- IAAPRAC/PAC3/src/exercise1.py
+++ IAAPRAC/PAC3/src/exercise1.py
@@ -1,39 +1,50 @@
 from PAC3.src.NaiveBayes import naiveBayes;
 from PAC3.src.DecisionTree import decisionTree;
+from PAC3.src.KNN import kNN;
 
-# Load file
-l = list(map(lambda l: (l.strip()).split(','),
+def readData():
+    # Load file
+    l = list(map(lambda l: (l.strip()).split(','),
             open('../data/Wholesale customers.csv', 'r').readlines()))
+    
+    for row in l:
+        row[1] = row[0] + '' + row[1]
+    
+    l = [row[1:] for row in l ]
+    
+    l = l[1:]
+    return l;
+    
+    
+print("RatioToTest, Naive bayes accuracy, Decision tree accuracy, kNNTreeAccuracy")
+for ratioToTest in range(2,20):
+    ######## Naive bayes 
 
-for row in l:
-    row[1] = 'Channel:' + row[0] + ' ' + 'Region: ' + row[1]
-
-l = [row[1:] for row in l ]
-
-ratioToTest=10
-
-######## Naive bayes 
-
-naiveBayesAccuracy, naiveBayesPredictions = naiveBayes(l, ratioTests=ratioToTest)
-
-# Print accuracy and predictions
-print('naiveBayesAccuracy. :', naiveBayesAccuracy , '%')
-
-######## Decision tree
-
-decisionTreeAccuracy,decisionTreePredictions = decisionTree(l,ratioToTest=ratioToTest)
-
-# Print accuracy and predictions
-print('decisionTreeAccuracy:', decisionTreeAccuracy, '%')
-print('decisionTreeAccuracy:', decisionTreePredictions)
-
-######## kNN
-
-
-
-######## Support vector machine Kernel 1
-
-
-######## Support vector machine Kernel 2
-
-
+    naiveBayesAccuracy, naiveBayesPredictions = naiveBayes(readData(), ratioTests=ratioToTest)
+    
+    # Print accuracy and predictions
+    # print('naiveBayesAccuracy. :', naiveBayesAccuracy , '%')
+    # print('naiveBayesPredictions:', naiveBayesPredictions)
+    
+    ######## Decision tree
+    
+    decisionTreeAccuracy,decisionTreePredictions = decisionTree(readData(),ratioToTest=ratioToTest)
+    
+    # Print accuracy and predictions
+    #print('decisionTreeAccuracy:', decisionTreeAccuracy, '%')
+    #print('decisionTreePredictions:', decisionTreePredictions)
+    
+    ######## kNN
+    
+    kNNAccuracy,kNNPredictions = kNN(readData(),ratioToTest=ratioToTest);
+    
+    # Print accuracy and predictions
+    #print('kNNTreeAccuracy:', kNNAccuracy, '%')
+    #print('kNNPredictions:', kNNPredictions)
+    
+    ######## Support vector machine Kernel 1
+    
+    
+    ######## Support vector machine Kernel 2
+    
+    print(ratioToTest,",",naiveBayesAccuracy,",",decisionTreeAccuracy,",", kNNAccuracy)
